00b5656786f860bb8e515947bee80516e0050754
Katie Unger <katie.e.unger@gmail.com>
2017-11-22 21:53:01 -0500

    adding bootstrap to dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Does this change?';
-  name = '';
+  //title = 'Does this change?';
+  //name = '';
 }
