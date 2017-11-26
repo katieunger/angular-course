a22a88decd10985dfc02ea013e5bfc9fe86b4253
Katie Unger <katie.e.unger@gmail.com>
2017-11-26 09:39:03 -0500

    styling app component HTML with Bootstrap elements; editing stylesheet for app component and then switching to inline styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`h3{color:dodgerblue}`]
 })
 export class AppComponent {
   //title = 'Does this change?';
