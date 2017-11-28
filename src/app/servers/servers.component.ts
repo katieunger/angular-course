cd010e16f4beba450efdb49d51a9724acf257610
Katie Unger <katie.e.unger@gmail.com>
2017-11-28 16:25:13 -0500

    updating servers component to use property binding to enable/disable add servers button

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,13 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
-  //templateUrl: './servers.component.html',
+  //template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  // Method executed at the time the component is created by Angular.
+  constructor() {
+    // ES6 arrow function
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
