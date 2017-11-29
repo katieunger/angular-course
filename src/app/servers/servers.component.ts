9a76bbb675b9da8bae6ff205fd8bd072782c26df
Katie Unger <katie.e.unger@gmail.com>
2017-11-29 10:27:06 -0500

    adding input and using input event to update server name by passing input's value using

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created!";
+  serverName = "";
 
   // Method executed at the time the component is created by Angular.
   constructor() {
@@ -23,6 +24,11 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = "Server was created!";
+  }
+
+  onUpdateServerName(event: Event){
+    // HTMLInputElement informs TypeScript that the type of the HTML element of the event is an input
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
