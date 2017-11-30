01999dd7c4057e12c0bace96b67a6760329bc356
Katie Unger <katie.e.unger@gmail.com>
2017-11-30 16:13:23 -0500

    displaying server name when add server button is clicked; organizing app component template and adding assignment details

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -23,7 +23,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = "Server was created!";
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
   }
 
   onUpdateServerName(event: Event){
