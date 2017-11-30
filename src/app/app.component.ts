c353c076bcebfd705b89caf5b0da37a6cc94ecff
Katie Unger <katie.e.unger@gmail.com>
2017-11-30 17:06:34 -0500

    working on assignment 2 - practicing databinding; adding username  input and paragraph below which displays input value using two-way databinding; adding reset username  button and onUpdateUsername event on input which enables button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
   styles: [`h3{color:dodgerblue}`]
 })
 export class AppComponent {
+  allowResetUsername = false;
+  username = '';
   //title = 'Does this change?';
   //name = '';
+
+  onUpdateUsername(event: Event){
+    // HTMLInputElement informs TypeScript that the type of the HTML element of the event is an input
+    console.log(this.username);
+    if (this.username !== ""){
+      this.allowResetUsername = true;
+    } else {
+      this.allowResetUsername = false;
+    }
+  }
 }
