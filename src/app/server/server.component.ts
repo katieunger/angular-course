eb2969dcf5cfaaad8802f4fb81075e0458f0b408
Katie Unger <katie.e.unger@gmail.com>
2017-12-01 15:49:59 -0500

    using ngStyle directive with property binding to change background color of server paragraph based on server being offline or online

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,15 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = "offline";
 
+  constructor(){
+    this.serverStatus = Math.random() > 0.5 ? "online" : "offline";
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor(){
+    return this.serverStatus === "online" ? "green" : "red";
   }
 }
