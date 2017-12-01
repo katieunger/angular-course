2530f792e925679dc59f87b5621ad9a4db23d9ef
Katie Unger <katie.e.unger@gmail.com>
2017-12-01 15:57:19 -0500

    changing classof server paragraph with ngClass directive to make text white when serverStatus is online

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+      .online {
+        color: white
+      }
+
+  `]
 })
 export class ServerComponent {
   serverId: number = 10;
