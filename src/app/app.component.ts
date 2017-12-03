a304c4ff6ace3593316c2cf650d01ecf9da3b6e3
Katie Unger <katie.e.unger@gmail.com>
2017-12-03 10:37:56 -0500

    working on assignment 3 - adding clicksArray and incrementing when display details button is clicked; using ngFor directive to display clicksArray as a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   allowResetUsername: boolean = false;
   username: string = '';
   showDetails: boolean = true;
+  clicksArray = [];
   //title = 'Does this change?';
   //name = '';
 
@@ -27,7 +28,18 @@ export class AppComponent {
     this.allowResetUsername = false;
   }
 
-  toggleDetails() {
+  onClickDetails() {
+    // Add counter to clicksArray
+    if (this.clicksArray.length == 0){
+      this.clicksArray.push(0)
+    } else {
+      // Last element in array
+      var lastItem = this.clicksArray[this.clicksArray.length - 1];
+      this.clicksArray.push(lastItem + 1);
+    }
+    console.log(this.clicksArray);
+
+    // Toggle showDetails boolean
     return this.showDetails ? this.showDetails = false : this.showDetails = true;
   }
 
