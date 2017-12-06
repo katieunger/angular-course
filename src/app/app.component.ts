978c3a83ae634ffec1abf8ae9d75efbb2caa6e63
Katie Unger <katie.e.unger@gmail.com>
2017-12-06 09:47:31 -0500

    setting up course project - removing sample code from assignments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,36 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  //styles: [`h3{color:dodgerblue} .white{color:white}`]
-
 })
+
 export class AppComponent {
-  allowResetUsername: boolean = false;
-  username: string = '';
-  showDetails: boolean = true;
-  clicksArray = [];
-  //title = 'Does this change?';
-  //name = '';
-
-  onUpdateUsername(event: Event){
-    console.log(this.username);
-    if (this.username !== ""){
-      this.allowResetUsername = true;
-    } else {
-      this.allowResetUsername = false;
-    }
-  }
-
-  onResetUsername() {
-    this.username = "";
-    this.allowResetUsername = false;
-  }
-
-  onClickDetails() {
-    // Toggle showDetails boolean
-    this.showDetails = !this.showDetails;
-
-    // Add counter to clicksArray
-    this.clicksArray.push(new Date());
-  }
+  title = 'Angular Project';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessComponent } from './success/success.component';
-import { WarningComponent } from './warning/warning.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessComponent,
-    WarningComponent
   ],
   imports: [
     BrowserModule,
