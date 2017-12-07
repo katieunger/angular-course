319e6a052ff4e32643dce655ae8ba5152bac5e44
Katie Unger <katie.e.unger@gmail.com>
2017-12-07 00:32:25 -0500

    removing navbar collapse; adding recipe model

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  recipes = [];
 
   constructor() { }
 
