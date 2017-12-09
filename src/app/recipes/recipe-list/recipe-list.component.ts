761e3321f148adf1460443bd7f6e31c5fb41f578
Katie Unger <katie.e.unger@gmail.com>
2017-12-09 16:13:57 -0500

    adding elements to shopping edit component template; adding headers to component templates to distinguish them

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
     new Recipe("A Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg")
   ];
 
