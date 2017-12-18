c1ac5ccdac26eb52ab316d0301c02dd573e5ddb0
Katie Unger <katie.e.unger@gmail.com>
2017-12-18 15:29:13 -0500

    aliasing blueprintCreated event; moving styling for paragraphs from app.component.css to server-element.component.css - understanding view encapsulation

--- components-databinding/src/app/cockpit/cockpit.component.ts
+++ components-databinding/src/app/cockpit/cockpit.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
+  @Output("bpCreated") blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
   newServerName = '';
   newServerContent = '';
 
