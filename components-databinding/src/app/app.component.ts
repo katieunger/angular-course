c852eb2df7bffff74ab6d29e6230530d6ae5de43
Katie Unger <katie.e.unger@gmail.com>
2017-12-18 14:54:56 -0500

    binding to custom events

--- components-databinding/src/app/app.component.ts
+++ components-databinding/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: "server", name: "Testserver", content: "Just a test!"}];
+
+  // Only executed after button is clicked
+  onServerAdded(serverData: {serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName:string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
--- components-databinding/src/app/cockpit/cockpit.component.ts
+++ components-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-
+  @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
   newServerName = '';
   newServerContent = '';
 
@@ -16,19 +17,17 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    //this.serverElements.push({
-    //  type: 'server',
-    //  name: this.newServerName,
-    //  content: this.newServerContent
-    //});
+    this.serverCreated.emit({
+      serverName:this.newServerName,
+      serverContent:this.newServerContent
+    })
   }
 
   onAddBlueprint() {
-    //this.serverElements.push({
-    //  type: 'blueprint',
-    //  name: this.newServerName,
-    //  content: this.newServerContent
-    //});
+    this.blueprintCreated.emit({
+      serverName:this.newServerName,
+      serverContent:this.newServerContent
+    })
   }
 
 }
