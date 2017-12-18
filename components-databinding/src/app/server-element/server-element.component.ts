fceab1ac80d2c7de412ced88f8751a29016fc790
Katie Unger <katie.e.unger@gmail.com>
2017-12-18 14:23:29 -0500

    assigning an alias to custom properties

--- components-databinding/src/app/server-element/server-element.component.ts
+++ components-databinding/src/app/server-element/server-element.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+  @Input("srvElement") element: {type: string, name: string, content: string};
 
   constructor() { }
 
