f18e143d8b7a365a7f8b26952c97c3d4b8f24263
Katie Unger <katie.e.unger@gmail.com>
2017-12-18 14:16:59 -0500

    binding to custom properties

--- components-databinding/src/app/app.component.ts
+++ components-databinding/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type: "server", name: "Testserver", content: "Just a test!"}];
 }
--- components-databinding/src/app/cockpit/cockpit.component.ts
+++ components-databinding/src/app/cockpit/cockpit.component.ts
@@ -16,19 +16,19 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
+    //this.serverElements.push({
+    //  type: 'server',
+    //  name: this.newServerName,
+    //  content: this.newServerContent
+    //});
   }
 
   onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
+    //this.serverElements.push({
+    //  type: 'blueprint',
+    //  name: this.newServerName,
+    //  content: this.newServerContent
+    //});
   }
 
 }
