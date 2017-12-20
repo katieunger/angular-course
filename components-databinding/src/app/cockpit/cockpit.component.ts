906114824bc34dbd1c6095c96df0c62f697d020c
Katie Unger <katie.e.unger@gmail.com>
2017-12-19 19:20:33 -0500

    using local references in templates; trying different methods of view encapsulation

--- components-databinding/src/app/cockpit/cockpit.component.ts
+++ components-databinding/src/app/cockpit/cockpit.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
   @Output("bpCreated") blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
-  newServerName = '';
+  //newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -16,16 +16,17 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
     this.serverCreated.emit({
-      serverName:this.newServerName,
+      serverName:nameInput.value,
       serverContent:this.newServerContent
     })
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName:this.newServerName,
+      serverName:nameInput.value,
       serverContent:this.newServerContent
     })
   }
--- components-databinding/src/app/server-element/server-element.component.ts
+++ components-databinding/src/app/server-element/server-element.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, Native
 })
 export class ServerElementComponent implements OnInit {
   @Input("srvElement") element: {type: string, name: string, content: string};
