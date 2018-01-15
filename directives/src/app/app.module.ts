528dd3c37d169e10707182efa6ad3c80c4b4f07f
Katie Unger <katie.e.unger@gmail.com>
2018-01-15 17:15:42 -0500

    creating a basic attribute directive

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ directives/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appBasicHighlight]'
+})
+
+export class BasicHighlightDirective implements OnInit {
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
+}
