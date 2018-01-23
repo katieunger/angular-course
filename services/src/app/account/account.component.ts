b35c8f57c8c038eeb7d6cc8f7c8fad4354802491
Katie Unger <katie.e.unger@gmail.com>
2018-01-23 15:02:10 -0500

    injecting the logging service into components

--- services/src/app/account/account.component.ts
+++ services/src/app/account/account.component.ts
@@ -1,18 +1,24 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import { LoggingService } from '../logging.service.ts'
+
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private loggingService: LoggingService) {
+
+  }
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
   }
 }
