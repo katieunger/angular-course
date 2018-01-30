f71958fad14c945905dcc1e72ea7765f402799ab
Katie Unger <katie.e.unger@gmail.com>
2018-01-29 22:11:54 -0500

    adding recipe service and shopping list service; managing recipes using recipe service

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
-import { Recipe } from "../recipe.model";
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,14 +10,13 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
-    new Recipe("Another Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg")
-  ];
 
-  constructor() { }
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
---
+++ course-project/src/app/recipes/recipe.service.ts
@@ -0,0 +1,17 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
+    new Recipe("Another Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg")
+  ];
+
+  getRecipes() {
+    // Using slice() with no arguments to return a new array
+    // rather than a reference to the original array
+    // which could mutate the array within the service.
+    return this.recipes.slice();
+  }
+
+}
