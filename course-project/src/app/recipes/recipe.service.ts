351aa506e1d837d6cddd6fc4ee8a1bb46dfde764
Katie Unger <katie.e.unger@gmail.com>
2018-01-31 20:58:30 -0500

    adding ingredients to recipe model

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
-    new Recipe("Another Test Recipe", "This is simply a test", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg")
+    new Recipe(
+      "Tasty Schnitzel",
+      "A super tasty Schnitzel - just awesome!",
+      "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG",
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      "Big Fat Burger",
+      "What else do you need to say?",
+      "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
   ];
 
   getRecipes() {
