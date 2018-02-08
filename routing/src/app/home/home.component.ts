915bd6ebb859a799ca3850d1a0511ed3deb93200
Katie Unger <katie.e.unger@gmail.com>
2018-02-08 15:22:24 -0500

    navigating programmatically by injecting router and using navigate method

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onLoadServers(){
+    // complex calculation
+    // Need to get access to router to navigate here
+    // Navigate method takes an argument which is an array of the different elements of the path
+    // We are using an absolute path
+    this.router.navigate(['/servers']);
   }
 
 }
