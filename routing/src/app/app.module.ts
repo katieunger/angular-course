4ae0e7947971c112803e1d7baa62f606e808df5f
Katie Unger <katie.e.unger@gmail.com>
2018-02-10 19:49:59 -0500

    passing parameters to routes - passing id to user component route

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ServersService } from './servers/servers.service';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
   { path: 'servers', component: ServersComponent }
 ];
 
