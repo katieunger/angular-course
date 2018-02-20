10d778a9370cac4a15d558654263196085f42b87
Katie Unger <katie.e.unger@gmail.com>
2018-02-20 17:11:35 -0500

    setting up child (nested) routes

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -15,11 +15,13 @@ import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UserComponent },
-  { path: 'servers', component: ServersComponent },
-  { path: 'servers/:id', component: ServerComponent },
-  { path: 'servers/:id/edit', component: EditServerComponent }
+  { path: 'users', component: UsersComponent, children: [
+    { path: ':id/:name', component: UserComponent },
+  ]},
+  { path: 'servers', component: ServersComponent, children: [
+    { path: ':id', component: ServerComponent },
+    { path: ':id/edit', component: EditServerComponent }
+  ]},
 ];
 
 @NgModule({
