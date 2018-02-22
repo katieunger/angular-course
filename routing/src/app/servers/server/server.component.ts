6375db3b3f0a49d264231121fa41d314d303c6e2
Katie Unger <katie.e.unger@gmail.com>
2018-02-22 17:02:34 -0500

    using query params to navigate to edit server page; passing allowEdit query params

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ServersService } from '../servers.service';
 
 @Component({
@@ -10,7 +10,7 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     // Plus sign below converts id from string to number
@@ -22,6 +22,10 @@ export class ServerComponent implements OnInit {
         this.server = this.serversService.getServer(+params['id']);
       }
     )
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route})
   }
 
 }
