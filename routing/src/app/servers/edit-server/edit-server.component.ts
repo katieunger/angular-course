0c1ed3d15b09557764797311b48bd65c357a7a0c
Katie Unger <katie.e.unger@gmail.com>
2018-02-26 22:51:54 -0500

    preserving query params when navigating using queryParamsHandling attribute; fixing issue with allowEdit

--- routing/src/app/servers/edit-server/edit-server.component.ts
+++ routing/src/app/servers/edit-server/edit-server.component.ts
@@ -21,7 +21,7 @@ export class EditServerComponent implements OnInit {
     this.route.queryParams
       .subscribe(
         (queryParams: Params) => {
-          this.allowEdit = queryParams['allowEdit'] === 1 ? true : false;
+          this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
         }
       );
     this.route.fragment.subscribe();
--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -25,7 +25,7 @@ export class ServerComponent implements OnInit {
   }
 
   onEdit() {
-    this.router.navigate(['edit'], {relativeTo: this.route})
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'})
   }
 
 }
