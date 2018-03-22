e480411600e7c97c15f65fa9c3a7394a3b882ee5
Katie Unger <katie.e.unger@gmail.com>
2018-03-22 15:42:31 -0400

    building and using a first simple observable

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,16 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Interval is a simple method for creating a new Observable
+    // We pass a number to interval as a parameter, and this specifies the number of milliseconds elapsed between emitting Observables
+    const myNumbers = Observable.interval(1000);
+    // Now that we have created the myNumbers Observable, we can subscribe to it
+    // Subscribe can be passed three callback functions: one for handling data, one for handling errors, and one for handling completion
+    // This Observable will not fail (raise an error) or complete
+    myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    )
   }
-
 }
