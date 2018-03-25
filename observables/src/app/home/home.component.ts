ec755b2d1e12d2315de23aec6986bcfb42c93dd0
Katie Unger <katie.e.unger@gmail.com>
2018-03-25 15:23:42 -0400

    adding map operator to numbers observable - trying out observable operators

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -18,7 +18,12 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit() {
     // Interval is a simple method for creating a new Observable
     // We pass a number to interval as a parameter, and this specifies the number of milliseconds elapsed between emitting Observables
-    const myNumbers = Observable.interval(1000);
+    const myNumbers = Observable.interval(1000)
+      .map(
+        (data: number) => {
+          return data * 2;
+        }
+    );
     // Now that we have created the myNumbers Observable, we can subscribe to it
     // Subscribe can be passed three callback functions: one for handling data, one for handling errors, and one for handling completion
     // This Observable will not fail (raise an error) or complete
