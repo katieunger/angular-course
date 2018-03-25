89e60d84c7d99beef8ea76ca93610253b7f822d7
Katie Unger <katie.e.unger@gmail.com>
2018-03-24 20:59:33 -0400

    using subjects to pass and listen to data

--- observables/src/app/user/user.component.ts
+++ observables/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user',
@@ -9,7 +10,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class UserComponent implements OnInit {
   id: number;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private usersService: UsersService) { }
 
   ngOnInit() {
     this.route.params
@@ -18,6 +19,11 @@ export class UserComponent implements OnInit {
           this.id = +params['id'];
         }
       );
+  }
+
+  onActivate() {
+    // Subject is Observable and Observer at the same time
+    this.usersService.userActivated.next(this.id);
   }
 
 }
