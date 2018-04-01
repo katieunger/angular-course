988321187f6264782fe21794a1235fc3204076a2
Katie Unger <katie.e.unger@gmail.com>
2018-04-01 14:58:17 -0400

    handling radio buttons

--- forms-template-driven/src/app/app.component.ts
+++ forms-template-driven/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
