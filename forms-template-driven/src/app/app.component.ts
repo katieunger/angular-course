b5919c4d2dac06ba4bbe455c0bb153713383bc65
Katie Unger <katie.e.unger@gmail.com>
2018-04-02 14:44:06 -0400

    resetting the form

--- forms-template-driven/src/app/app.component.ts
+++ forms-template-driven/src/app/app.component.ts
@@ -49,5 +49,7 @@ export class AppComponent {
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
