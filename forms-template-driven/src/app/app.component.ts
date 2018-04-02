686f9dc61be00056fc108443f9588b50fdee1559
Katie Unger <katie.e.unger@gmail.com>
2018-04-02 14:41:52 -0400

    using form data - displaying submitted data below form

--- forms-template-driven/src/app/app.component.ts
+++ forms-template-driven/src/app/app.component.ts
@@ -10,6 +10,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -34,6 +42,12 @@ export class AppComponent {
   //}
 
   onSubmit() {
-    console.log(this.signupForm);
+    //console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
