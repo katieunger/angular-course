d8649be09bf266bdb9f1d63d66754baa1c82ba56
Katie Unger <katie.e.unger@gmail.com>
2018-04-04 23:05:42 -0400

    working on adding form elments to assignment

--- forms-td-assignment/src/app/app.component.ts
+++ forms-td-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+@ViewChild('f') testForm: NgForm;
+
+  onSubmit() {
+    console.log(this.testForm);
+  }
 
 }
