5764ccfdb12d8d4dc3a44b29241c189b7a95fa24
Katie Unger <katie.e.unger@gmail.com>
2018-04-05 15:48:29 -0400

    completing requirements for assignment; trying to get ng-class to work to apply Bootstrap has-error class

--- forms-td-assignment/src/app/app.component.ts
+++ forms-td-assignment/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,10 +8,23 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-@ViewChild('f') testForm: NgForm;
+  @ViewChild('f') testForm: NgForm;
+  defaultSubscription = 'advanced';
+  user = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  submitted = false;
 
   onSubmit() {
-    console.log(this.testForm);
+    //console.log(this.testForm);
+    this.submitted = true;
+    this.user.email = this.testForm.value.email;
+    this.user.subscription = this.testForm.value.subscription;
+    this.user.password = this.testForm.value.password;
+
+    this.testForm.reset();
   }
 
 }
