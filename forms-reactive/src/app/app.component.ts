368fe6fc9230135bd735221cdb571baf302625a1
Katie Unger <katie.e.unger@gmail.com>
2018-04-18 15:12:13 -0400

    adding email validator

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, ]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male')
     });
   }
