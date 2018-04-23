0b9f09e895c303768420ea4b27b011bb5fcbd376
Katie Unger <katie.e.unger@gmail.com>
2018-04-23 15:48:47 -0400

    creating a custom async validator

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,13 @@ export class AppComponent implements OnInit {
   signupForm: FormGroup;
   forbiddenUsernames = ['Chris', 'Anna'];
 
+
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
           // Need to bind 'this' because when Angular executes forbiddenNames function this will not be signupForm
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -41,5 +43,18 @@ export class AppComponent implements OnInit {
     }
     return null;
     // If validation is successful you must return nothing or null
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve,reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden':true})
+        } else {
+          resolve(null)
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
