e7711c4fdcb652669e7731d6974122e4e58ae07f
Katie Unger <katie.e.unger@gmail.com>
2018-04-27 17:11:42 -0400

    creating reactive form and adding project name and email form controls

--- forms-reactive-assignment/src/app/app.component.ts
+++ forms-reactive-assignment/src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  forbiddenProjectNames = ['Test'];
+
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+      'email': new FormControl(null, [Validators.required, Validators.email])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+    this.projectForm.reset();
+  }
+
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean}{
+    // If control.value is not in the array, this is evaluated as -1
+    // -1 is interpreted as true!
+    if(this.forbiddenProjectNames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    return null;
+    // If validation is successful you must return nothing or null
+  }
+
 }
