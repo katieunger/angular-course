0b34576d3b77982a914db309da7e4d3ef965b211
Katie Unger <katie.e.unger@gmail.com>
2018-05-01 17:12:05 -0400

    working on adding async validator for project name

--- forms-reactive-assignment/src/app/app.component.ts
+++ forms-reactive-assignment/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+      'projectName': new FormControl(null, [Validators.required], this.forbiddenNames.bind(this)),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'status': new FormControl('stable')
     });
@@ -22,18 +22,31 @@ export class AppComponent implements OnInit {
 
   onSubmit() {
     console.log(this.projectForm);
-    this.projectForm.reset();
+    //this.projectForm.reset();
   }
 
 
-  forbiddenNames(control: FormControl): {[s: string]: boolean}{
-    // If control.value is not in the array, this is evaluated as -1
-    // -1 is interpreted as true!
-    if(this.forbiddenProjectNames.indexOf(control.value) !== -1){
-      return {'nameIsForbidden': true};
-    }
-    return null;
-    // If validation is successful you must return nothing or null
+  //forbiddenNames(control: FormControl): {[s: string]: boolean}{
+  //  // If control.value is not in the array, this is evaluated as -1
+  //  // -1 is interpreted as true!
+  //  if(this.forbiddenProjectNames.indexOf(control.value) !== -1){
+  //    return {'nameIsForbidden': true};
+  //  }
+  //  return null;
+  //  // If validation is successful you must return nothing or null
+  //}
+
+  forbiddenNames(control: FormControl): Promise<any> | Observable<any> {
+  const promise = new Promise<any>((resolve,reject) => {
+    setTimeout(() => {
+      if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+        resolve({'nameIsForbidden':true})
+      } else {
+        resolve(null)
+      }
+    }, 1500);
+  });
+  return promise;
   }
 
 }
