62e3753a57941c0288d8176b805f6bdac20876f9
Katie Unger <katie.e.unger@gmail.com>
2018-05-02 17:53:33 -0400

    switching from errors array to hasError property

--- forms-reactive-assignment/src/app/app.component.ts
+++ forms-reactive-assignment/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null, [Validators.required], this.forbiddenNames.bind(this)),
+      'projectName': new FormControl(null, [Validators.required], this.asyncForbiddenNames.bind(this)),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'status': new FormControl('stable')
     });
@@ -26,27 +26,27 @@ export class AppComponent implements OnInit {
   }
 
 
-  //forbiddenNames(control: FormControl): {[s: string]: boolean}{
-  //  // If control.value is not in the array, this is evaluated as -1
-  //  // -1 is interpreted as true!
-  //  if(this.forbiddenProjectNames.indexOf(control.value) !== -1){
-  //    return {'nameIsForbidden': true};
-  //  }
-  //  return null;
-  //  // If validation is successful you must return nothing or null
-  //}
+  forbiddenNames(control: FormControl): {[s: string]: boolean}{
+    // If control.value is not in the array, this is evaluated as -1
+    // -1 is interpreted as true!
+    if(this.forbiddenProjectNames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    return null;
+    // If validation is successful you must return nothing or null
+  }
 
-  forbiddenNames(control: FormControl): Promise<any> | Observable<any> {
-  const promise = new Promise<any>((resolve,reject) => {
-    setTimeout(() => {
-      if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
-        resolve({'nameIsForbidden':true})
-      } else {
-        resolve(null)
-      }
-    }, 1500);
-  });
-  return promise;
+  asyncForbiddenNames(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+          resolve({'nameIsForbidden': true})
+        } else {
+          resolve(null)
+        }
+      }, 1500);
+    });
+    return promise;
   }
 
 }
