a057ec03971c70c9bfe811d01790dadd6558a45b
Katie Unger <katie.e.unger@gmail.com>
2018-05-27 18:46:56 -0400

    creating a filter pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -32,6 +32,7 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
+  filteredStatus = '';
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
