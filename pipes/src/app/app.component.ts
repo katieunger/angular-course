1b2a8638059c6fce85cdfeba61a9dbf4c026d2ee
Katie Unger <katie.e.unger@gmail.com>
2018-05-30 16:18:27 -0400

    adding async pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
   servers = [
     {
       instanceType: 'medium',
