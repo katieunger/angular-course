f4afe54bbc8760edabcb716c7e68dfce4eaa39b5
Katie Unger <katie.e.unger@gmail.com>
2018-06-05 15:30:15 -0400

    sending requests - POST request

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServerService } from './server.service';
 
 @Component({
   selector: 'app-root',
@@ -18,12 +19,20 @@ export class AppComponent {
       id: this.generateId()
     }
   ];
+  constructor(private serverService: ServerService){}
   onAddServer(name: string) {
     this.servers.push({
       name: name,
       capacity: 50,
       id: this.generateId()
     });
+  }
+  onSave() {
+    this.serverService.storeServers(this.servers)
+      .subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+    );
   }
   private generateId() {
     return Math.round(Math.random() * 10000);
