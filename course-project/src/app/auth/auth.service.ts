883fbc5e9d53f47afc2cbcbfc9e84cefb8aa9fc0
Katie Unger <katie.e.unger@gmail.com>
2018-06-27 16:20:05 -0400

    checking and using authentication status

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -32,4 +32,8 @@ export class AuthService {
       );
     return this.token;
   }
+
+  isAuthenticated() {
+    return this.token != null;
+  }
 }
--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Response } from '@angular/http';
 
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,10 @@ import { DataStorageService } from '../shared/data-storage.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
