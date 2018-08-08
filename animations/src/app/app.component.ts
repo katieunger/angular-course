a738b471e9c55ae5a414071b4a2abe0e50d73aa6
Katie Unger <katie.e.unger@gmail.com>
2018-08-07 23:11:07 -0400

    using animation callbacks

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -133,4 +133,12 @@ export class AppComponent {
   onDelete(item) {
     this.list.splice(this.list.indexOf(item),1);
   }
+
+  animationStarted(event) {
+    console.log(event);
+  }
+
+  animationEnded(event) {
+    console.log(event);
+  }
 }
