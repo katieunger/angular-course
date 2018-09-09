553a56a5d62f4ce9431bc6574ecc725a3cf2f7bf
Katie Unger <katie.e.unger@gmail.com>
2018-09-09 12:02:15 -0400

    reporting progress with HttpRequest

--- httpclient/src/app/shared/data-storage.service.ts
+++ httpclient/src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
@@ -17,12 +17,22 @@ export class DataStorageService {
     const token = this.authService.getToken();
     //const headers = new HttpHeaders().set('Authorization', 'Bearer asdkaghlkajd');
 
-    return this.httpClient.put('https://ng-recipe-book-ec4c6.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
-      //observe: 'events'
-      observe: 'body',
-      params: new HttpParams().set('auth', token)
-      //headers: headers
-    });
+    //return this.httpClient.put('https://ng-recipe-book-ec4c6.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+    //  //observe: 'events'
+    //  observe: 'body',
+    //  params: new HttpParams().set('auth', token)
+    //  //headers: headers
+    //});
+
+    const req = new HttpRequest('PUT',
+      'https://ng-recipe-book-ec4c6.firebaseio.com/recipes.json',
+      this.recipeService.getRecipes(),
+      {
+        reportProgress: true,
+        params: new HttpParams().set('auth', token)
+      })
+
+    return this.httpClient.request(req)
   }
 
   getRecipes() {
