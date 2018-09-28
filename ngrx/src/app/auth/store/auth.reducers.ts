6da7e24c5cf997edeb20ec3499711a9136a599ea
Katie Unger <katie.e.unger@gmail.com>
2018-09-28 11:03:10 -0400

    adding auth reducer logic and actions

--- ngrx/src/app/auth/store/auth.reducers.ts
+++ ngrx/src/app/auth/store/auth.reducers.ts
@@ -1,3 +1,5 @@
+import * as AuthActions from './auth.actions';
+
 export interface State {
   token: string;
   authenticated: boolean;
@@ -8,7 +10,21 @@ const initialState: State = {
   authenticated: false
 };
 
-export function authReducer(state = initialState, action) {
-
-  return state;
+export function authReducer(state = initialState, action AuthActions.AuthActions) {
+  switch (action.type) {
+    case (AuthActions.SIGNUP):
+    case (AuthActions.SIGNIN):
+      return {
+        ...state,
+        authenticated: true
+      };
+    case (AuthActions.LOGOUT):
+      return {
+        ...state,
+        token: null,
+        authenticated: false
+      };
+    default:
+      return state;
+  }
 }
