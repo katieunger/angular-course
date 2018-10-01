76e49d09d06df381c2581454c77a9f0e3ffd38fd
Katie Unger <katie.e.unger@gmail.com>
2018-09-30 23:30:57 -0400

    using authentication

--- ngrx/src/app/core/header/header.component.ts
+++ ngrx/src/app/core/header/header.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 // import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 import { DataStorageService } from '../../shared/data-storage.service';
 import { AuthService } from '../../auth/auth.service';
+import * as fromApp from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  authState: Observable<fromAuth.State>;
+
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService) {
+              private authService: AuthService
+              private store: Store<fromApp.AppState>) {
+  }
+
+  ngOnInit() {
+    this.authState = this.store.select('auth');
   }
 
   onSaveData() {
