4561d29b152865a37f73c8d816e6cea854fa0372
Katie Unger <katie.e.unger@gmail.com>
2018-10-03 16:06:16 -0400

    getting state access in http interceptor

--- ngrx/src/app/auth/auth-guard.service.ts
+++ ngrx/src/app/auth/auth-guard.service.ts
@@ -1,14 +1,18 @@
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
 
-import { AuthService } from './auth.service';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from './store/auth.reducers';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.authService.isAuthenticated();
+    return this.store.select('auth').map((authState: fromAuth.State) => {
+      return authState.authenticated;
+    });
   }
 }
--- ngrx/src/app/shared/auth.interceptor.ts
+++ ngrx/src/app/shared/auth.interceptor.ts
@@ -1,18 +1,27 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
-import { AuthService } from '../auth/auth.service';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted!', req);
     // const copiedReq = req.clone({headers: req.headers.set('', '')});
-    const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
-    return next.handle(copiedReq);
+    return this.store.select('auth')
+      .pipe(switchMap((authState: fromAuth.State) => {
+        const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
+        return next.handle(copiedReq);
+      }));
+
+
     // return null;
   }
 }
