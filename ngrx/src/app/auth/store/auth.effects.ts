5f6a958e7fc76654342b4be352d372ea04a3cb63
Katie Unger <katie.e.unger@gmail.com>
2018-10-15 16:56:21 -0400

    adding auth signup

--- ngrx/src/app/auth/store/auth.effects.ts
+++ ngrx/src/app/auth/store/auth.effects.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/mergeMap';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import * as firebase from 'firebase';
 
 import * as AuthActions from './auth.actions';
 
@@ -9,7 +14,27 @@ export class AuthEffects {
   @Effect()
   // ofType() is a helper method provided by ngrx/effects
   authSignup = this.actions$
-    .ofType(AuthActions.TRY_SIGNUP);
+    .ofType(AuthActions.TRY_SIGNUP)
+    .map((action: AuthActions.TrySignup) => {
+      return action.payload;
+    })
+    .switchMap((authData: {username: string, password: string}) => {
+      return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+    })
+    .switchMap(() => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      return [
+        {
+          type: AuthActions.SIGNUP
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
+    });
 
   // Using $ to indicate that actions is an Observable
   // actions$ is just like a list of all the actions we have in our app
