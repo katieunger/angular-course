3ddcac11c37a64c2bde0e770af56c558bd96d627
Katie Unger <katie.e.unger@gmail.com>
2018-10-19 23:43:06 -0400

    additional fixes

--- ngrx/src/app/auth/auth-guard.service.ts
+++ ngrx/src/app/auth/auth-guard.service.ts
@@ -11,7 +11,9 @@ export class AuthGuard implements CanActivate {
   constructor(private store: Store<fromApp.AppState>) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.store.select('auth').map((authState: fromAuth.State) => {
+    return this.store.select('auth')
+      .take(1)
+      .map((authState: fromAuth.State) => {
       return authState.authenticated;
     });
   }
