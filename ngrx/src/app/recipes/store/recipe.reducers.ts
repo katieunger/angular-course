e14bb0f72b2ef797554ef510e16519dce9aa0cfb
Katie Unger <katie.e.unger@gmail.com>
2018-10-26 14:23:08 -0400

    adding recipe reducers

--- ngrx/src/app/recipes/store/recipe.reducers.ts
+++ ngrx/src/app/recipes/store/recipe.reducers.ts
@@ -1,5 +1,6 @@
 import { Recipe } from '../recipe.model';
 import { Ingredient } from '../../shared/ingredient.model';
+import * as RecipeActions from './recipe.actions';
 
 export interface FeatureState {
   recipes: State
@@ -29,7 +30,39 @@ const initialState: State = {
   ]
 };
 
+export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions) {
+  switch (action.type) {
+    case (RecipeActions.SET_RECIPES):
+      return {
+        ...state,
+          recipes: [...action.payload]
+      };
+    case (RecipeActions.ADD_RECIPE):
+      return {
+        ...state,
+        recipes: [...state.recipes, action.payload]
+      };
+    case (RecipeActions.UPDATE_RECIPE):
+      const recipe = state.recipes[action.payload.index];
+      const updatedRecipe = {
+        ...recipe,
+        ...action.payload.updatedRecipe
+      }
+      const recipes = [...state.recipes];
+      recipes[action.payload.index] = updatedRecipe;
+      return {
+        ...state,
+        recipes: recipes
+      };
+    case (RecipeActions.DELETE_RECIPE):
+      const oldRecipes = [...state.recipes];
+      oldRecipes.splice(action.payload, 1);
+      return {
+        ...state,
+          recipes: oldRecipes
+      };
+    default:
+      return state;
+  }
 
-export function recipeReducer(state = initialState, action) {
-  return state
 }
